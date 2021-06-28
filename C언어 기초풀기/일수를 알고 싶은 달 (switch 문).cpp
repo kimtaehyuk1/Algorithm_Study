36a7029857053fd9bc8d0d78274d546b983e922e
김태혁 <67897827+kimtaehyuk1@users.noreply.github.com>
2021-06-28 15:50:37 +0900

    Create 일수를 알고 싶은 달 (switch 문).cpp

---
+++ C언어 기초풀기/일수를 알고 싶은 달 (switch 문).cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(void) {
+	
+	int month, days;
+	
+	printf("일수를 알고싶은 달을 입력하세요: ");
+	scanf("%d", &month);
+	
+	switch (month) 
+	{
+		case 2:
+		
+			days = 28;
+			break;
+		
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			days = 30;
+			break;
+			
+		default:
+			days = 31;
+			break;			
+	}
+
+
+	printf("%d 월의 일수는 %d입니다.", month, days);
+	return 0;
+}
